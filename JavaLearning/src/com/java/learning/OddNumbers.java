208a77bc87e9df0eb6d2303d58c7e6673c7857e8
santosh248 <santhosh0248@gmail.com>
2020-07-16 16:48:57 +0530

    Learnings

---
+++ JavaLearning/src/com/java/learning/OddNumbers.java
@@ -0,0 +1,16 @@
+package com.java.learning;
+public class OddNumbers {
+
+	public static void main(String[] args) {
+     int x=1;
+     do {
+    	 if(x%2==1){
+    		 System.out.println(x);
+    	 }
+    	x++	 ;
+    	 
+     }while(x<=30);
+    	 
+	}
+
+}
---
+++ JavaLearning/src/com/java/learning/StringReverse.java
@@ -0,0 +1,11 @@
+package com.java.learning;
+public class StringReverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ StringBuffer str =new StringBuffer("Hello World");
+ str.reverse();
+ System.out.println(str);
+	}
+
+}
